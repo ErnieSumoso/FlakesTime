091cc10dd4a273b166182d98db492e44f4668e63
ErnieSumoso <144075326+ErnieSumoso@users.noreply.github.com>
2023-09-26 22:59:53 -0500

    dev: change the polls application model

--- FlakesTime/polls/models.py
+++ FlakesTime/polls/models.py
@@ -8,8 +8,8 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    question_text = models.CharField(max_length=100)
+    pub_date = models.DateTimeField("Date Published")
 
     def __str__(self):
         return self.question_text
